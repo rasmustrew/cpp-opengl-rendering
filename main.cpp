2a98ef4de3aaa3c83c5a6dad68623d4c1100ac9c
Rasmus Thorsøe <rt071195@gmail.com>
2026-02-07 17:19:18 +0100

    multiple cubes rendered

--- main.cpp
+++ main.cpp
@@ -66,7 +66,18 @@ int main() {
 
 		glEnable(GL_DEPTH_TEST);
 
-
+		glm::vec3 cubePositions[] = {
+			glm::vec3(0.0f,  0.0f,  0.0f),
+			glm::vec3(2.0f,  5.0f, -15.0f),
+			glm::vec3(-1.5f, -2.2f, -2.5f),
+			glm::vec3(-3.8f, -2.0f, -12.3f),
+			glm::vec3(2.4f, -0.4f, -3.5f),
+			glm::vec3(-1.7f,  3.0f, -7.5f),
+			glm::vec3(1.3f, -2.0f, -2.5f),
+			glm::vec3(1.5f,  2.0f, -2.5f),
+			glm::vec3(1.5f,  0.2f, -1.5f),
+			glm::vec3(-1.3f,  1.0f, -1.5f)
+		};
 
 		// -------------- Render loop --------------
 
@@ -74,15 +85,23 @@ int main() {
 
 			processInput(window);
 
+			shaderProgram.use();
 
 			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 			glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-			shaderProgram.use();
+			for (unsigned int i = 0; i < 10; i++) {
+				model = glm::mat4(1.0f);
+				model = glm::translate(model, cubePositions[i]);
+				float angle = 20.0f * i;
+				model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+				shaderProgram.setMat4("model", model);
+				box.draw();
+			}
 
-			model = glm::mat4(1.0f);
-			model = glm::rotate(model, (float)glfwGetTime() * glm::radians(50.0f), glm::vec3(0.5f, 1.0f, 0.0f));
-			shaderProgram.setMat4("model", model);
+
+
+
 
 			box.draw();
 
